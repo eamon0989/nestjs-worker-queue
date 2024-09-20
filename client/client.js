b7b1fbc4bd02fd3d7c90f4c646a564b6a0d56f94
eamon0989 <eamon0989@gmail.com>
2024-09-20 14:52:05 +0200

    chore: run prettier

--- client/client.js
+++ client/client.js
@@ -1,12 +1,11 @@
-
 // Store for all of the jobs in progress
 let jobs = {};
 
 // Kick off a new job by POST-ing to the server
 async function addJob() {
-  let res = await fetch('job/', {method: 'POST'});
+  let res = await fetch('job/', { method: 'POST' });
   let job = await res.json();
-  jobs[job.id] = {id: job.id, state: "queued"};
+  jobs[job.id] = { id: job.id, state: 'queued' };
   render();
 }
 
@@ -30,7 +29,7 @@ function clear() {
 
 // Update the UI
 function render() {
-  let s = "";
+  let s = '';
   for (let id of Object.keys(jobs)) {
     s += renderJob(jobs[id]);
   }
@@ -38,34 +37,34 @@ function render() {
   // For demo simplicity this blows away all of the existing HTML and replaces it,
   // which is very inefficient. In a production app a library like React or Vue should
   // handle this work
-  document.querySelector("#job-summary").innerHTML = s;
+  document.querySelector('#job-summary').innerHTML = s;
 }
 
 // Renders the HTML for each job object
 function renderJob(job) {
   let progress = job.progress || 0;
-  let color = "bg-light-purple";
+  let color = 'bg-light-purple';
 
-  if (job.state === "completed") {
-    color = "bg-purple";
+  if (job.state === 'completed') {
+    color = 'bg-purple';
     progress = 100;
-  } else if (job.state === "failed") {
-    color = "bg-dark-red";
+  } else if (job.state === 'failed') {
+    color = 'bg-dark-red';
     progress = 100;
   }
-  
-  return document.querySelector('#job-template')
-    .innerHTML
-    .replace('{{id}}', job.id)
+
+  return document
+    .querySelector('#job-template')
+    .innerHTML.replace('{{id}}', job.id)
     .replace('{{state}}', job.state)
     .replace('{{color}}', color)
     .replace('{{progress}}', progress);
 }
 
 // Attach click handlers and kick off background processes
-window.onload = function() {
-  document.querySelector("#add-job").addEventListener("click", addJob);
-  document.querySelector("#clear").addEventListener("click", clear);
+window.onload = function () {
+  document.querySelector('#add-job').addEventListener('click', addJob);
+  document.querySelector('#clear').addEventListener('click', clear);
 
   setInterval(updateJobs, 200);
 };
